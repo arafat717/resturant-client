e6b616018cb0394c3cecaafc471d1dc5fd6b2250
arafat717 <arafatjibon33@gmail.com>
2025-06-28 22:49:41 +0600

    added sall

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,7 +7,6 @@ const gallery = [breakfast, breakfast, breakfast, breakfast];
 const Banner = () => {
   return (
     <section className="bg-[#880808] text-white w-full h-screen relative overflow-hidden">
-      {/* Logo and Search */}
       <div className="flex justify-between items-center  pt-12 px-20">
         <div>
           <h1 className="text-white font-bold text-[32px]">RESTAURANT</h1>
@@ -22,7 +21,6 @@ const Banner = () => {
         </div>
       </div>
       <div className="flex flex-col lg:flex-row  px-28 py-16 relative z-10">
-        {/* Left Content */}
         <div className="w-full  space-y-6 relative">
           <h2 className="text-[96px]">BREAKFAST</h2>
           <p className="text-[20px] font-semibold leading-6 w-[900px]">
@@ -32,13 +30,9 @@ const Banner = () => {
             and proteins, that contribute to a balanced diet.
           </p>
 
-          {/* Image Carousel */}
           <div className="flex items-center space-x-4 mt-6">
             {gallery.map((src, idx) => (
-              <div
-                key={idx}
-                className="rounded-full overflow-hidden border-4 border-transparent hover:border-white transition duration-300"
-              >
+              <div key={idx}>
                 <Image
                   src={src}
                   alt={`breakfast${idx + 1}`}
@@ -50,7 +44,6 @@ const Banner = () => {
           </div>
         </div>
 
-        {/* Right Image */}
         <div className="w-full lg:w-1/2 mt-10 lg:mt-0 flex justify-center items-center">
           <Image
             src={breakfast}
@@ -61,7 +54,6 @@ const Banner = () => {
         </div>
       </div>
 
-      {/* Decorative Circle */}
       <div className="absolute top-0 left-0 w-[600px] h-[600px] bg-red-800 rounded-full opacity-40 -z-10" />
       <div className="absolute bottom-0 right-0 w-[400px] h-[400px] bg-red-800 rounded-full opacity-30 -z-10" />
     </section>
